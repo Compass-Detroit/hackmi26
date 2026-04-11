0ea9793c27857d74e5e7a73369bab88c9adcee68
Greg Miller <hello@gregmiller.io>
2026-04-11 18:07:21 -0400

    refactor: add navigation

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * External campaign URLs (IBM Agentic / HackMI).
+ * Replace placeholders when final links are published.
+ */
+export const HACKER_REGISTRATION_URL =
+  'https://www.ibm.com/';
+
+export const SPONSOR_INQUIRY_URL =
+  'https://www.ibm.com/';
+
+/** Bottom-of-page and repeat CTAs to the primary Agentic / registration destination. */
+export const IBM_AGENTIC_CAMPAIGN_URL = HACKER_REGISTRATION_URL;
