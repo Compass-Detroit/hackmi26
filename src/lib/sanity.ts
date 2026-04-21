2a0a3b5f5d03d21674211879f7fab3bec6f74c56
Greg Miller <hello@gregmiller.io>
2026-04-21 15:57:40 -0400

    feat: setup sanity methods

---
+++ src/lib/sanity.ts
@@ -0,0 +1,59 @@
+import { sanityClient } from "sanity:client";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+const builder = imageUrlBuilder(sanityClient);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
+
+export async function getPosts() {
+  return await sanityClient.fetch(
+    `*[_type == "post"] | order(pubDate desc)`
+  );
+}
+
+export async function getProjects() {
+  return await sanityClient.fetch(
+    `*[_type == "project"] | order(featured desc, title asc)`
+  );
+}
+
+export async function getTeams() {
+  return await sanityClient.fetch(
+    `*[_type == "team"] {
+      ...,
+      "projects": *[_type == "hackathonProject" && references(^._id)]
+    } | order(name asc)`
+  );
+}
+
+export async function getTeam(slug: string) {
+  return await sanityClient.fetch(
+    `*[_type == "team" && slug.current == $slug][0] {
+      ...,
+      "projects": *[_type == "hackathonProject" && references(^._id)]
+    }`,
+    { slug }
+  );
+}
+
+export async function getHackathonProjects() {
+  return await sanityClient.fetch(
+    `*[_type == "hackathonProject"] {
+      ...,
+      team->
+    } | order(title asc)`
+  );
+}
+
+export async function getHackathonProject(slug: string) {
+  return await sanityClient.fetch(
+    `*[_type == "hackathonProject" && slug.current == $slug][0] {
+      ...,
+      team->
+    }`,
+    { slug }
+  );
+}
