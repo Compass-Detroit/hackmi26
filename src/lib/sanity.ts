228b3ca0c2dd3cebae3508326bb0e25eea322fe8
Greg Miller <hello@gregmiller.io>
2026-04-22 09:27:00 -0400

    refactor: remove blog and project
    - installation of Sanity Studio enabled a hackathon path
    - the blog and project templates are not needed
    - system-wide removal of blog and project templates

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -8,18 +8,6 @@ export function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
 
-export async function getPosts() {
-  return await sanityClient.fetch(
-    `*[_type == "post"] | order(pubDate desc)`
-  );
-}
-
-export async function getProjects() {
-  return await sanityClient.fetch(
-    `*[_type == "project"] | order(featured desc, title asc)`
-  );
-}
-
 export async function getTeams() {
   return await sanityClient.fetch(
     `*[_type == "team"] {
