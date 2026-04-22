0444515a80c0e116fe6d243c723c68fc757c2c85
Greg Miller <hello@gregmiller.io>
2026-04-22 09:43:03 -0400

    docs: remove from feature list
    - remove blog, project and about
    - remove types, templates
    -

--- studio-hack-michigan-/schemaTypes/index.ts
+++ studio-hack-michigan-/schemaTypes/index.ts
@@ -1,6 +1,4 @@
-import {postType} from './post'
-import {projectType} from './project'
 import {teamType} from './team'
 import {hackathonProjectType} from './hackathonProject'
 
-export const schemaTypes = [postType, projectType, teamType, hackathonProjectType]
+export const schemaTypes = [teamType, hackathonProjectType]
